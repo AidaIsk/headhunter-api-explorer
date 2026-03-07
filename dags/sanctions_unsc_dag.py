7d3f6128a0f664e96ef995fee0f62a0ba7123ce0
Aida <aida.aitymova@gmail.com>
2026-03-07 18:50:10 +0100

    feat: implement DAG for UNSC sanctions ingestion

---
+++ dags/sanctions_unsc_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+from utils.sanctions_utils import ingest_unsc
+
+default_args = {
+    "owner": "risk-team",
+    "start_date": datetime(2024,1,1),
+    "retries": 1
+}
+
+with DAG(
+    dag_id="ingest_unsc_sanctions",
+    schedule_interval="0 21 * * 0",
+    catchup=False,
+    default_args=default_args
+) as dag:
+
+    ingest_task = PythonOperator(
+        task_id="download_unsc_list",
+        python_callable=ingest_unsc
+    )
