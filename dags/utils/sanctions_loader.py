4eae012320da4aeee27f4a07f87c33a3616cca7c
Aida <aida.aitymova@gmail.com>
2026-03-14 09:11:12 +0100

    refactor: change target schema from silver to dbt_staging
    Updated sanctions_loader to land parsed data in staging area for further dbt transformations.

--- dags/utils/sanctions_loader.py
+++ dags/utils/sanctions_loader.py
@@ -15,7 +15,7 @@ def generate_entity_hash(entity):
     return hashlib.sha256(text.encode()).hexdigest()
 
 
-def load_unsc_to_silver(parsed_data):
+def load_unsc_to_staging(parsed_data):
 
     postgres = PostgresHook(postgres_conn_id="postgres_bronze")
     conn = postgres.get_conn()
@@ -31,8 +31,6 @@ def load_unsc_to_silver(parsed_data):
 
     for e in parsed_data["entities"]:
 
-        entity_hash = generate_entity_hash(e)
-
         entities_rows.append((
             e["entity_id"],
             e.get("version"),
@@ -40,29 +38,20 @@ def load_unsc_to_silver(parsed_data):
             e.get("name"),
             e.get("reference_number"),
             e.get("listed_on"),
-            e.get("gender"),
-            e.get("comments"),
-            entity_hash,
-            now,
-            None,
-            True
+            "UN_SC_1267",
+            now
         ))
 
     sql_entities = """
-    INSERT INTO silver.sanctions_entities
-    (
-        dataid,
-        version,
+    INSERT INTO dbt_staging.stg_unsc__entities (
+        entity_id,
+        data_id,
         entity_type,
-        name,
-        reference_number,
+        primary_name,
+        un_reference_number,
         listed_on,
-        gender,
-        comments,
-        entity_hash,
-        valid_from,
-        valid_to,
-        is_current
+        source_list,
+        load_timestamp
     )
     VALUES %s
     """
@@ -73,96 +62,96 @@ def load_unsc_to_silver(parsed_data):
     # ALIASES
     # ------------------------
 
-    aliases_rows = []
+    # aliases_rows = []
 
-    for a in parsed_data["aliases"]:
+    # for a in parsed_data["aliases"]:
 
-        aliases_rows.append((
-            a["entity_id"],
-            a["alias"],
-            now,
-            None,
-            True
-        ))
+    #     aliases_rows.append((
+    #         a["entity_id"],
+    #         a["alias"],
+    #         now,
+    #         None,
+    #         True
+    #     ))
 
-    sql_aliases = """
-    INSERT INTO silver.sanctions_aliases
-    (
-        dataid,
-        alias,
-        valid_from,
-        valid_to,
-        is_current
-    )
-    VALUES %s
-    """
+    # sql_aliases = """
+    # INSERT INTO silver.sanctions_aliases
+    # (
+    #     dataid,
+    #     alias,
+    #     valid_from,
+    #     valid_to,
+    #     is_current
+    # )
+    # VALUES %s
+    # """
 
-    if aliases_rows:
-        execute_values(cursor, sql_aliases, aliases_rows)
+    # if aliases_rows:
+    #     execute_values(cursor, sql_aliases, aliases_rows)
 
-    # ------------------------
-    # NATIONALITIES
-    # ------------------------
+    # # ------------------------
+    # # NATIONALITIES
+    # # ------------------------
 
-    nat_rows = []
+    # nat_rows = []
 
-    for n in parsed_data["nationalities"]:
+    # for n in parsed_data["nationalities"]:
 
-        nat_rows.append((
-            n["entity_id"],
-            n["nationality"],
-            now,
-            None,
-            True
-        ))
+    #     nat_rows.append((
+    #         n["entity_id"],
+    #         n["nationality"],
+    #         now,
+    #         None,
+    #         True
+    #     ))
 
-    sql_nat = """
-    INSERT INTO silver.sanctions_nationalities
-    (
-        dataid,
-        nationality,
-        valid_from,
-        valid_to,
-        is_current
-    )
-    VALUES %s
-    """
+    # sql_nat = """
+    # INSERT INTO silver.sanctions_nationalities
+    # (
+    #     dataid,
+    #     nationality,
+    #     valid_from,
+    #     valid_to,
+    #     is_current
+    # )
+    # VALUES %s
+    # """
 
-    if nat_rows:
-        execute_values(cursor, sql_nat, nat_rows)
+    # if nat_rows:
+    #     execute_values(cursor, sql_nat, nat_rows)
 
-    # ------------------------
-    # DOCUMENTS
-    # ------------------------
+    # # ------------------------
+    # # DOCUMENTS
+    # # ------------------------
 
-    doc_rows = []
+    # doc_rows = []
 
-    for d in parsed_data["documents"]:
+    # for d in parsed_data["documents"]:
 
-        doc_rows.append((
-            d["entity_id"],
-            d["doc_type"],
-            d["doc_number"],
-            now,
-            None,
-            True
-        ))
+    #     doc_rows.append((
+    #         d["entity_id"],
+    #         d["doc_type"],
+    #         d["doc_number"],
+    #         now,
+    #         None,
+    #         True
+    #     ))
 
-    sql_docs = """
-    INSERT INTO silver.sanctions_documents
-    (
-        dataid,
-        doc_type,
-        doc_number,
-        valid_from,
-        valid_to,
-        is_current
-    )
-    VALUES %s
-    """
+    # sql_docs = """
+    # INSERT INTO silver.sanctions_documents
+    # (
+    #     dataid,
+    #     doc_type,
+    #     doc_number,
+    #     valid_from,
+    #     valid_to,
+    #     is_current
+    # )
+    # VALUES %s
+    # """
 
-    if doc_rows:
-        execute_values(cursor, sql_docs, doc_rows)
+    # if doc_rows:
+    #     execute_values(cursor, sql_docs, doc_rows)
 
     conn.commit()
 
